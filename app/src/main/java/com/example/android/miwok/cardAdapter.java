2dec46788ec48bc71c1dd74f6e3fc95980ff301b
ishita sharma <ishitasharma12699@gmail.com>
2017-10-23 23:12:57 +0530

    adding circular reveal transition to card views

--- app/src/main/java/com/example/android/miwok/cardAdapter.java
+++ app/src/main/java/com/example/android/miwok/cardAdapter.java
@@ -1,15 +1,20 @@
 package com.example.android.miwok;
 
+import android.animation.Animator;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -70,14 +75,15 @@ class cardAdapter extends RecyclerView.Adapter<cardAdapter.ViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, final int position) {
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
               Word word= wordArrayList.get(position);
         holder.imageView.setImageResource(word.getImageResourceid());
         holder.textView.setText(word.getMiwok());
-        //holder.textView.setTextColor(ContextCompat.getColor(context,colorid));
+        holder.englishTextView.setText(word.getDefaultWord());
+        //holder.englishTextView.setTextColor(ContextCompat.getColor(context,colorid));
         holder.cardView.setBackgroundColor(ContextCompat.getColor(context,colorid));
         //recyclerView.setBackgroundColor(ContextCompat.getColor(context,colorid));
-        holder.cardView.setOnClickListener(new View.OnClickListener() {
+        holder.linearlayoutInCardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 releseMediaPlayer();
@@ -97,22 +103,72 @@ class cardAdapter extends RecyclerView.Adapter<cardAdapter.ViewHolder> {
                     });
             }
         }
-    });
+        });
+        holder.linearlayoutInCardView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                animate(view,holder);
+                return true;
+            }
+        });
     }
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void animate(final View view, final ViewHolder holder) {
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP) {
+        int cx= holder.englishTextView.getWidth()/2;
+        int cy= holder.englishTextView.getHeight()/2;
+        float finalRadius = (float)Math.hypot(cx,cy);
+
+            Animator animator = ViewAnimationUtils.createCircularReveal(view, cx, cy, 0, finalRadius);
+
+            animator.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animator) {
+                    //view.setVisibility(View.GONE);
+                    //
+                    //holder.englishTextView.setVisibility(View.VISIBLE);
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animator) {
+                    holder.englishTextView.setVisibility(View.VISIBLE);
+                    view.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animator) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animator) {
+                    view.setVisibility(View.VISIBLE);
+                    holder.englishTextView.setVisibility(View.GONE);
+                }
+            });
+            animator.setDuration(3000);
+            animator.start();
+        }
+    }
+
     @Override
     public int getItemCount() {
         return wordArrayList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
-        public TextView textView;
+        public TextView textView,englishTextView;
         public ImageView imageView;
         public CardView cardView;
+        public LinearLayout linearlayoutInCardView;
+
         public ViewHolder(View itemView) {
             super(itemView);
             textView = (TextView) itemView.findViewById(R.id.text_1_view);
             imageView=(ImageView) itemView.findViewById(R.id.image_view);
             cardView=(CardView) itemView.findViewById(R.id.card_view);
+            englishTextView=(TextView)itemView.findViewById(R.id.english_textView);
+            linearlayoutInCardView=(LinearLayout)itemView.findViewById(R.id.linear_in_card_layout);
         }
     }
 }
